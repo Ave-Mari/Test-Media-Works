5f2039e80377b8e58be0c6b2faf950fa6ececf02
Mari Vaypan <avemari98@gmail.com>
2021-06-06 22:15:13 +0300

    added burger-menu and adaptive layouts

---
+++ js/burger-menu.js
@@ -0,0 +1,18 @@
+function burgerClick() {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const burgerMenuList = document.querySelector('.nav-burger');
+        if ("" === burgerMenu.getAttribute("data-state")) {
+            burgerMenu.setAttribute("data-state", "active");
+        } else {
+            burgerMenu.setAttribute("data-state", "");
+        }
+
+
+        if ("active" === burgerMenu.getAttribute("data-state")) {
+            burgerMenuList.style.left = '61%';
+        } else {
+            burgerMenuList.style.left = '-9999999999%';
+        }
+
+        
+}
